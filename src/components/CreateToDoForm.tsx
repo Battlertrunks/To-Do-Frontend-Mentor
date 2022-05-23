4c97d4d1ac26c029ced296426292f299f72fde72
Gavin <Gavinszczesniak@gmail.com>
2022-05-23 18:53:56 -0400

    Initial push, frontend mentorfiles, and starting components.

---
+++ src/components/CreateToDoForm.tsx
@@ -0,0 +1,7 @@
+import "./CreateToDoForm.css";
+
+const CreateToDoForm = () => {
+  return <div className="CreateToDoForm">CreateToDoForm works</div>;
+};
+
+export default CreateToDoForm;
---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,7 @@
+import "./ToDoList.css";
+
+const ToDoList = () => {
+  return <div className="ToDoList">ToDoList works</div>;
+};
+
+export default ToDoList;
