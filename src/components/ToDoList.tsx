e042ad6d058c6b55583d3869cedfa3e80a6c15d0
Gavin <Gavinszczesniak@gmail.com>
2022-05-24 13:31:10 -0400

    Users can filter the list of seeing all, completed, or not completed from the buttons below. Users can also clear/delete completed tasks all at once, and on the bottom users can see how many tasks are left to complete.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -9,14 +9,16 @@ const ToDoList = () => {
     { toDoSentence: "Clean the house", completed: true },
   ]);
 
-  const recreateArray = (index: number) => {
+  const [filter, setFilter] = useState<boolean | null>(null);
+
+  const recreateArray = (index: number): void => {
     const newArr: ToDo[] = [...toDos];
     newArr[index].completed = !newArr[index].completed;
 
     setToDos(newArr);
   };
 
-  const deleteTask = (index: number) => {
+  const deleteTask = (index: number): void => {
     const newArr: ToDo[] = [
       ...toDos.slice(0, index),
       ...toDos.slice(index + 1),
@@ -25,23 +27,57 @@ const ToDoList = () => {
     setToDos(newArr);
   };
 
-  const addNewTask = (task: ToDo) => {
+  const addNewTask = (task: ToDo): void => {
     setToDos([task, ...toDos]);
+  };
+
+  const filterList = (toDo: ToDo): boolean => {
+    if (filter === null) return true;
+    else if (filter === toDo.completed && filter) return true;
+    else if (filter === toDo.completed && !filter) return true;
+    return false;
+  };
+
+  const clearCompleted = (): void => {
+    setToDos([...toDos.filter((toDo) => !toDo.completed)]);
   };
 
   return (
     <section className="ToDoList">
       <CreateToDoForm addTask={addNewTask} />
       <ul>
-        {toDos.map((toDo, i) => (
-          <li key={i}>
-            <button onClick={() => recreateArray(i)}>
-              {toDo.completed ? "Completed" : "Complete"}
-            </button>
-            <p>{toDo.toDoSentence}</p>
-            <button onClick={() => deleteTask(i)}>X</button>
-          </li>
-        ))}
+        {toDos
+          .filter((toDo) => filterList(toDo))
+          .map((toDo, i) => (
+            <li key={i}>
+              <button onClick={() => recreateArray(i)}>
+                {toDo.completed ? "Completed" : "Complete"}
+              </button>
+              <p>{toDo.toDoSentence}</p>
+              <button onClick={() => deleteTask(i)}>X</button>
+            </li>
+          ))}
+      </ul>
+      <ul>
+        <li>
+          <p>{toDos.filter((toDo) => !toDo.completed).length} items left</p>
+        </li>
+        <li>
+          <ul>
+            <li>
+              <button onClick={() => setFilter(null)}>All</button>
+            </li>
+            <li>
+              <button onClick={() => setFilter(false)}>Active</button>
+            </li>
+            <li>
+              <button onClick={() => setFilter(true)}>Completed</button>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <button onClick={() => clearCompleted()}>Clear Completed</button>
+        </li>
       </ul>
     </section>
   );
